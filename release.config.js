deb36fd236716d55bf62cc44b1f2e1d8240ae641
mghirvu <mghirvu@deloittece.com>
2022-10-14 13:01:52 +0300

    fix: dw

--- release.config.js
+++ release.config.js
@@ -5,11 +5,14 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    ["@semantic-release/github", {
+    [
+      "@semantic-release/github",
+      {
         assets: [
-            { path: "build.zip", label: "Build"},
-            { path: "coverage.zip", label: "Coverage"}
-        ]
-    }],
+          { path: "build.zip", label: "Build" },
+          { path: "coverage.zip", label: "Coverage" },
+        ],
+      },
+    ],
   ],
 };
