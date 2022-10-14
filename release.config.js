adea71708182c9bcd668950fc16884b9544f3d65
mghirvu <mghirvu@deloittece.com>
2022-10-14 11:57:32 +0300

    fix: fix something

---
+++ release.config.js
@@ -0,0 +1,10 @@
+// https://github.com/semantic-release/semantic-release
+module.exports = {
+    branches: "master",
+    repositoryUrl: "https://github.com/madalin34/react-app-actions",
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/github"
+    ]
+}
